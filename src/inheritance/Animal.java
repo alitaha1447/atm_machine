b70eba6b5db25c2a83c23bfcd7d85d8cc788ba5a
alitaha1447 <135825805+alitaha1447@users.noreply.github.com>
2023-06-11 12:55:43 +0530

    Created Inheritance Project

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Animal {
+    public String name;
+    public int age;
+    public void speak(){
+        System.out.println("Animal Speak : " + "Name : " + name + " " + age);
+    }
+}
---
+++ src/inheritance/MainClass.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class MainClass {
+    public static void main(String[] args){
+        Animal animal = new  Animal();
+        animal.name = "German";
+        animal.age = 12;
+        animal.speak();
+
+    }
+}
