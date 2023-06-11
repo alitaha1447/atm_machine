f4757e517a4fdd20c27fefa7a8ec31e5c85e60ff
alitaha1447 <135825805+alitaha1447@users.noreply.github.com>
2023-06-11 13:49:52 +0530

    Static/taha/practise

---
+++ src/staticDemo/Car.java
@@ -0,0 +1,12 @@
+package staticDemo;
+
+public class Car {
+    String car_name;
+    int price;
+    static String company_name = "TATA";
+    public void call(){
+        System.out.println("Car Name : " + " " + car_name);
+        System.out.println("Car price : " + " " + price);
+        System.out.println("Company Name : " + " " + company_name);
+    }
+}
---
+++ src/staticDemo/MainClass.java
@@ -0,0 +1,20 @@
+package staticDemo;
+
+public class MainClass {
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.car_name = "TATA Safari";
+        car1.price = 850000;
+        car1.call();
+        System.out.println();
+        Car car2 = new Car();
+        car2.car_name = "TATA Sumo";
+        car2.price = 3500070;
+        car2.call();
+        System.out.println();
+        Car car3 = new Car();
+        car3.car_name = "TATA Honda";
+        car3.price = 5200000;
+        car3.call();
+    }
+}
