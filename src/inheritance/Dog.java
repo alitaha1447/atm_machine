3ebd7c9dc6e1e10a78d0947ff4e1735431573b86
alitaha1447 <135825805+alitaha1447@users.noreply.github.com>
2023-06-11 13:05:57 +0530

    Implemented upcasting

--- src/inheritance/Dog.java
+++ src/inheritance/Dog.java
@@ -1,5 +1,10 @@
 package inheritance;
 
 public class Dog extends Animal{
-
+public void speak(){
+    System.out.println("Dog is speaking!!");
+}
+public void walk(){
+    System.out.println("Dog is walking!!");
+}
 }
--- src/inheritance/MainClass.java
+++ src/inheritance/MainClass.java
@@ -2,10 +2,12 @@ package inheritance;
 
 public class MainClass {
     public static void main(String[] args){
-        Animal animal = new  Animal();
+        Animal animal = new  Dog(); //upcasting
         animal.name = "German";
         animal.age = 12;
         animal.speak();
+        Dog dog = new Dog();
+        dog.walk();
 
     }
 }
