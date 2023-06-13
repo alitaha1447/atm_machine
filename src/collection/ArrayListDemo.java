1237e57b03d0c5f74d2f6c87c290ea2bf30573fb
alitaha1447 <ali.taha1447@gmail.com>
2023-06-13 23:57:34 +0530

    user/taha/array/changes

--- src/collection/ArrayListDemo.java
+++ src/collection/ArrayListDemo.java
@@ -1,7 +1,29 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ArrayListDemo {
     public static void main(String[] args){
+        Student s1 = new Student(101,"Taha",29);
+        Student s2 = new Student(102,"Ali",30);
+        Student s3 = new Student(103,"Shabbir",31);
+        ArrayList<Student> a1 = new ArrayList<>();
+        a1.add(s1);
+        a1.add(s2);
+        a1.add(s3);
+//Traversing using foreach loop
+        for (Student st:a1
+             ) {
+            System.out.println(st.rollNo + st.name + st.age);
+        }
+
+//        Iterator it = a1.iterator();
+//
+//        while (it.hasNext()){
+//            Student st = (Student) it.next();
+//            System.out.println(st.rollNo + " " + st.name + " " + st.age);
+//        }
 
     }
 }
